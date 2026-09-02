560de676b4a3f68a847bfe82070db77e0918c933
asdiAdi <carltimothyadi@gmail.com>
2026-09-02 21:18:06 +0800

    init

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          files = {
+            hidden = true,
+            -- ignored = true,
+          },
+          explorer = {
+            hidden = true,
+            -- ignored = true,
+          },
+        },
+        exclude = { "node_modules" },
+      },
+    },
+  },
+}
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/which-key.nvim",
+    opts = {
+      win = {
+        no_overlap = false,
+        row = 0.9,
+        col = 0.5,
+        width = 0.75,
+      },
+    },
+  },
+}
