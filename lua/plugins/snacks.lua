97328aaa0529c908bd3853154416546d469ce754
asdiAdi <carltimothyadi@gmail.com>
2026-09-02 22:29:20 +0800

    combine snacks to 1 file

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -19,5 +19,51 @@ return {
         exclude = { "node_modules" },
       },
     },
+    keys = {
+      {
+        "gd",
+        function()
+          if #vim.lsp.get_clients({ bufnr = 0 }) == 0 then
+            vim.notify("No LSP detected", vim.log.levels.WARN)
+            return
+          end
+          Snacks.picker.lsp_definitions({ jump1 = false })
+        end,
+        desc = "Goto Definition",
+      },
+      {
+        "gr",
+        function()
+          if #vim.lsp.get_clients({ bufnr = 0 }) == 0 then
+            vim.notify("No LSP detected", vim.log.levels.WARN)
+            return
+          end
+          Snacks.picker.lsp_references({ jump1 = false })
+        end,
+        desc = "References",
+      },
+      {
+        "gI",
+        function()
+          if #vim.lsp.get_clients({ bufnr = 0 }) == 0 then
+            vim.notify("No LSP detected", vim.log.levels.WARN)
+            return
+          end
+          Snacks.picker.lsp_implementations({ jump1 = false })
+        end,
+        desc = "Goto Implementation",
+      },
+      {
+        "gy",
+        function()
+          if #vim.lsp.get_clients({ bufnr = 0 }) == 0 then
+            vim.notify("No LSP detected", vim.log.levels.WARN)
+            return
+          end
+          Snacks.picker.lsp_type_definitions({ jump1 = false })
+        end,
+        desc = "Goto Type Definition",
+      },
+    },
   },
 }
