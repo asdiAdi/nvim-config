0e5145265914dda61a205d02bbfc917e77627f55
asdiAdi <carltimothyadi@gmail.com>
2026-09-25 22:42:10 +0800

    fix clean install error

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # 💤 LazyVim
 
-A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
-Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+My Neovim setup for web work. Based on [LazyVim](https://github.com/LazyVim/LazyVim).
+
+## Install
+
+```bash
+git clone https://github.com/anomalyco/nvim-config.git ~/.config/nvim
+nvim
+```
+
+Open nvim and everything sets itself up. Open a file and give it a minute
+the first time while it downloads what it needs.
--- lua/plugins/web-lsp.lua
+++ lua/plugins/web-lsp.lua
@@ -1,5 +1,4 @@
 return {
-  -- Mason: ensure common web servers installed + auto-install on FileType with notify
   {
     "mason-org/mason.nvim",
     opts = function(_, opts)
@@ -17,14 +16,6 @@ return {
     end,
   },
   {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      automatic_installation = true,
-    },
-  },
-
-  -- Treesitter: ensure parsers + auto_install
-  {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
@@ -39,52 +30,6 @@ return {
         "tsx",
         "typescript",
         "yaml",
-      })
-      opts.auto_install = true
-    end,
-  },
-
-  -- Notify + auto-install when opening unknown filetype
-  {
-    "neovim/nvim-lspconfig",
-    opts = function()
-      local ft_to_server = {
-        typescript = "vtsls",
-        javascript = "vtsls",
-        typescriptreact = "vtsls",
-        javascriptreact = "vtsls",
-        html = "html",
-        css = "cssls",
-        scss = "cssls",
-        json = "jsonls",
-        jsonc = "jsonls",
-        yaml = "yamlls",
-      }
-
-      vim.api.nvim_create_autocmd("FileType", {
-        group = vim.api.nvim_create_augroup("web_lsp_auto_install", { clear = true }),
-        callback = function(args)
-          local ft = args.match
-          local server = ft_to_server[ft]
-          if not server then
-            return
-          end
-          if #vim.lsp.get_clients({ bufnr = args.buf }) > 0 then
-            return
-          end
-          local ok, registry = pcall(require, "mason-registry")
-          if not ok then
-            return
-          end
-          if not registry.has_package(server) then
-            return
-          end
-          local pkg = registry.get_package(server)
-          if not pkg:is_installed() then
-            vim.notify("Installing LSP for " .. ft .. " (" .. server .. ")...", vim.log.levels.INFO)
-            pkg:install()
-          end
-        end,
       })
     end,
   },
